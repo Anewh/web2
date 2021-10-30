5ca15925d88efc48224e24c6e7ff51aac4a372f5
Anewh <i0ianrusi0i@gmail.com>
2021-10-30 23:53:12 +0300

    капча в комментах, вывод в консоль

--- js/popups.js
+++ js/popups.js
@@ -1,35 +1,69 @@
-var openModalWindow = document.getElementById('sign-in'),
-    closeModalWindow = document.getElementById('popup-close'),
-    chooseToSignIn = document.getElementById('sign-in-form-btn'),
-    chooseToSignUp = document.getElementById('sign-up-form-btn');
+var popup = document.getElementById('popup'), 
+    formReg = document.getElementById('form-reg'),
+    formIn = document.getElementById('form-in');
 
-// Отслеживаем клик
-openModalWindow.addEventListener('click', function() {
+document.getElementById('sign-in').addEventListener('click', function() {
   popup.classList.add('open');
-  document.getElementById('form-reg').classList.add('open');
+  formReg.classList.add('open');
 });
 
-closeModalWindow.addEventListener('click', function() {
+document.getElementById('popup-close').addEventListener('click', function() {
   document.getElementById('popup').classList.remove('open');
-  if(document.getElementById('form-reg').classList.contains('open')){
-    document.getElementById('form-reg').classList.remove('open');
+  if(formReg.classList.contains('open')){
+    formReg.classList.remove('open');
   }
-  if(document.getElementById('form-in').classList.contains('open')){
-    document.getElementById('form-in').classList.remove('open');
+  if(formIn.classList.contains('open')){
+    formIn.classList.remove('open');
   }
 });
 
-chooseToSignIn.addEventListener('click', function(){
-  if(document.getElementById('form-reg').classList.contains('open')){
-    document.getElementById('form-reg').classList.remove('open');
+document.getElementById('sign-in-form-btn').addEventListener('click', function(){
+  if(formReg.classList.contains('open')){
+    formReg.classList.remove('open');
   }
-  document.getElementById('form-in').classList.add('open');
+  formIn.classList.add('open');
 });
 
-chooseToSignUp.addEventListener('click', function(){
-  if(document.getElementById('form-in').classList.contains('open')){
-    document.getElementById('form-in').classList.remove('open');
+document.getElementById('sign-up-form-btn').addEventListener('click', function(){
+  if(formIn.classList.contains('open')){
+    formIn.classList.remove('open');
   }
-  document.getElementById('form-reg').classList.add('open');
+  formReg.classList.add('open');
 });
 
+
+function password_repeat_validation(password, password_repeat) {
+    if(password==password_repeat){
+        return true;
+    }
+    alert('Passwords must be equals');
+    return false;
+}
+
+
+function formRegValidation(){
+    if(formReg.classList.contains('open')){
+        var name = formReg.name.value;
+        var password = formReg.password.value;
+        var password_repeat = formReg.repeat.value;
+        var email =formReg.email.value;
+        var phone = formReg.phone.value;
+        if(password===password_repeat){
+            console.log('Name: ' + name + 
+              '\nPassword: ' + password + 
+              '\nRepeat password: '+ password_repeat +
+              '\nEmail: '+email +
+              '\nPhone number: '+ phone);
+        }
+    }    
+    return false;
+}
+
+function formInValidation(){
+    if(formIn.classList.contains('open')){
+        var password = formIn.password.value;
+        var email = formIn.email.value;
+        console.log('Email: '+ email + '\nPassword: ' + password);
+    }    
+    return false;
+}
